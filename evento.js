becfdcfa51355c27e2e54d6e609799f32699ff62
AbigailTrass <sailorflv@hotmail.com>
2023-08-12 16:58:21 -0300

    index.html y evento.js
    Le agregue el onclick al elemento html, id y cree el evento de js

---
+++ evento.js
@@ -0,0 +1,6 @@
+let boton =document.getElementById("btn");
+let parrafo =document.getElementById("parrafo");
+
+boton.addEventListener('click',function(){
+ parrafo.innerHTML = 'Hola! soy el div'
+})
